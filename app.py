0950f884a185919e7411469e512db36174c5e177
bhavani0131 <durgabhavaniv77@gmail.com>
2026-05-25 11:09:01 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+# app.py
+
+system_type = "Linux"
+port = 1000
+
+print("Starting Server...")
+print("System Type:", system_type)
+print("Port Number:", port)
+
+status = "Running"
+print("Server Status:", status)
+print("Application Started Successfully")
